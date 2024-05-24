2041ac306b74f64a1eaa3ec5439e32e4379df87d
Muhammad Qois Huzyan Octava <qoisoctava@gmail.com>
2024-05-24 15:03:17 +0700

    13 - m23

--- 13-nodejs-web-server/app.js
+++ 13-nodejs-web-server/app.js
@@ -1,10 +1,36 @@
 const http = require("http");
+const fs = require("fs");
+const port = 3000;
+
+const renderHTML = (path, res) => {
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.write("Error File Not Found!");
+    } else {
+      res.write(data);
+    }
+    res.end();
+  });
+};
 
 http
   .createServer((req, res) => {
-    res.write('Hello World...');
-    res.end()
+    const url = req.url;
+
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    });
+    if (url === "/") {
+      renderHTML("./html/index.html", res);
+    } else if (url === "/contact") {
+      renderHTML("./html/contact.html", res);
+    } else if (url === "/about") {
+      renderHTML("./html/about.html", res);
+    } else {
+      renderHTML("./html/else.html", res);
+    }
   })
-  .listen(3000, () => {
-    console.log("listening on port 3000...");
+  .listen(port, () => {
+    console.log(`listening on port ${port}...`);
   });
