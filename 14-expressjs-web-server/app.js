f0ee222fd2e9a25fa6e1ea7fb3beda7b1f737c23
Muhammad Qois Huzyan Octava <qoisoctava@gmail.com>
2024-05-24 21:10:04 +0700

    14 - m24

--- 14-expressjs-web-server/app.js
+++ 14-expressjs-web-server/app.js
@@ -3,65 +3,51 @@ const app = express()
 
 const port = 3000
 
+//EJS
+app.set('view engine', 'ejs')
+
+
 app.get('/', (req,res)=>{
-  res.sendFile('./html/index.html', {root: __dirname})
+  const politisi = [
+    {
+      nama: 'Jokowi',
+      partai: 'PDIP'
+    },
+    {
+      nama: 'Anies',
+      partai: 'Non Partai'
+    },
+    {
+      nama: 'Bamsoet',
+      partai: 'Golkar'
+    },
+    {
+      nama: 'Syahroni',
+      partai: 'Nasdem'
+    },
+    {
+      nama: 'Imin',
+      partai: 'PKB'
+    },
+  ]
+
+
+  res.render('index',{ nama: 'Sadhika', title: 'Home', politisi: politisi})
 })
 
 app.get('/about', (req,res)=>{
-  res.sendFile('./html/about.html', {root: __dirname})
+  res.render('about')
 })
 
 app.get('/contact', (req,res)=>{
-  res.sendFile('./html/contact.html', {root: __dirname})
+  res.render('contact')
 })
 
 app.use('/', (req,res)=>{
   res.status(404)
-  res.sendFile('./html/else.html', {root: __dirname})
+  res.render('else')
 })
 
 app.listen(port, ()=>{
   console.log(`App listening at http://localhost/${port}`)
 })
-
-
-
-
-
-// const http = require("http");
-// const fs = require("fs");
-// const port = 3000;
-
-// const renderHTML = (path, res) => {
-//   fs.readFile(path, (err, data) => {
-//     if (err) {
-//       res.writeHead(404);
-//       res.write("Error File Not Found!");
-//     } else {
-//       res.write(data);
-//     }
-//     res.end();
-//   });
-// };
-
-// http
-//   .createServer((req, res) => {
-//     const url = req.url;
-
-//     res.writeHead(200, {
-//       "Content-Type": "text/html",
-//     });
-//     if (url === "/") {
-//       renderHTML("./html/index.html", res);
-//     } else if (url === "/contact") {
-//       renderHTML("./html/contact.html", res);
-//     } else if (url === "/about") {
-//       renderHTML("./html/about.html", res);
-//     } else {
-//       renderHTML("./html/else.html", res);
-//     }
-    
-//   })
-//   .listen(port, () => {
-//     console.log(`listening on port ${port}...`);
-//   });
